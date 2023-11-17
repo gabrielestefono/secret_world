5c5a1d11075814d6f35dba3dc8a68e2e8b814832
gabrielestefono <gabrielestefono@hotmail.com>
2023-11-17 06:51:39 -0300

    first commit

---
+++ src/data/words.js
@@ -0,0 +1,8 @@
+export const wordlist = {
+	carro: ["Motor", "Porta", "Capô", "Pneu", "Antena"],
+	fruta: ["Maçã", "Banana", "Pera", "Uva", "Melancia"],
+	corpo: ["Braço", "Perna", "Cabeça", "Mão", "Pé"],
+	computador: ["Teclado", "Mouse", "Monitor", "CPU", "Impressora"],
+	progrmacao: ["Javascript", "Python", "Java", "C#", "PHP"],
+	alimenntos: ["Arroz", "Feijão", "Macarrão", "Carne", "Peixe"],
+};
